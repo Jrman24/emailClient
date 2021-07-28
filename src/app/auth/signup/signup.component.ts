c9214f07e676618a22cf7db97be08fc78bc8b85c
Oscar M <manatajr@gmail.com>
2021-07-28 08:24:50 -0400

    Sign up authentication service updates

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,39 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {MatchPassword} from '../validators/match-password';
 import {UniqueUsername} from '../validators/unique-username';
+import {AuthService} from '../auth.service';
+
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  authForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/)
-    ], [this.uniqueUserName.validate]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-      Validators.maxLength(8),
-    ]),
-    passwordConfirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-      Validators.maxLength(8),
-    ])
-  }, {validators: [this.matchPassWord.validate]});
+    authForm = new FormGroup({
+        username: new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(20),
+            Validators.pattern(/^[a-z0-9]+$/)
+        ], [this.uniqueUserName.validate]),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(8),
+        ]),
+        passwordConfirmation: new FormControl('', [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(8),
+        ])
+    }, {validators: [this.matchPassWord.validate]});
 
-  constructor(
-    private matchPassWord: MatchPassword,
-    private uniqueUserName: UniqueUsername
-  ) { console.log(this.authForm); }
+    constructor(
+        private matchPassWord: MatchPassword,
+        private uniqueUserName: UniqueUsername,
+        private authService: AuthService
+    ) {
+        console.log(this.authForm);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onSubmit() {
+        if (this.authForm.invalid) {
+            return;
+        }
+        this.authService.signUp(this.authForm.value).
+        subscribe({
+            next:  response => {
+
+            },
+            error: (err) => {
+                if (!err.status) {
+                    this.authForm.setErrors({ noConnection: true });
+                } else {
+                    this.authForm.setErrors({ unknownError: true });
+                }
+            }
+        });
+    }
 }
